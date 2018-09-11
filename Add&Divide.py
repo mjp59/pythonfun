42fc9c8328bae2971f6751d1681ff69c28446720
Michael Postiglione <mjp59@duke.edu>
2018-09-11 15:49:50 -0400

    task

---
+++ Add&Divide.py
@@ -0,0 +1,14 @@
+def main():
+ 	ans = divide_bythree(add_three(3, 5, 10))
+ 	print(ans)
+
+def add_three(v1, v2, v3):
+	*** do something
+	***
+ 	p = v1 + v2 + v3
+ 
+def divide_bythree(v1):
+	p=v1/3
+
+if_name_==*_main_*:
+	 main()
---
+++ fun.py
@@ -0,0 +1,29 @@
+def main():
+ add_two(3, 5)
+ subtract_two(2, 1)
+ multiply_two(5, 5)
+
+def add_two(v1, v2):
+ *** do something
+ ***
+ p = v1 + v2
+ print(p)
+
+def subtract_two(v1, v2):
+ *** do more
+ ***
+ p = v1 - v2
+ print(p)
+
+def multiply_two(v1, v2):
+ *** multiply two numbers
+
+ :param v1: number one
+ :param v2:number two
+ :returns product number
+ ***
+ p = v1 * v2
+ print(p)
+
+if_name_==*_main_*:
+ main()
